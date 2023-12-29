95337ed7942d4e5ea98732ddc8b04a242bc27d49
swayamtejwani <swayam.tejwani@gmail.com>
2023-12-29 12:38:32 +0530

    footer changes

--- wp-content/themes/hjm/footer.php
+++ wp-content/themes/hjm/footer.php
@@ -1,89 +1,37 @@
-<?php
-
-$uploadsDirectoryArray = wp_upload_dir();
-$uploadsDirectory = $uploadsDirectoryArray['url'];
-
-$objDateTime = new DateTime('NOW');
-$year = $objDateTime->format('Y');
-
-$displayCredit = '<small id="credit" class="first last"><a href="https://fwdlabs.com/" target="_blank" rel="noopener">Site by FWD:labs</a></small>';
-
-$subscribeForm = do_shortcode('[mc4wp_form id=1188]');
-
-$displaySubscribeForm = "";
-if (!is_page('subscribe')) {
-	$displaySubscribeForm = <<<EOD
-<div class="column-flex-1"><div class="gutter">
-$subscribeForm
-</div></div>
-EOD;
-}
-
-$siteName = get_bloginfo( 'name' );
-$siteDescription = get_bloginfo( 'description' );
-
-$displayFooterMenu1 = "<ul>";
-$footerMenuItems = wp_get_nav_menu_items( 'Primary Nav' );
-foreach ( $footerMenuItems as $menuItem ) {
-	$menuItemTitle = $menuItem->title;
-	$menuItemPermalink = $menuItem->url;
-	$displayFooterMenu1 .= '<li><a href="'.$menuItemPermalink.'">'.$menuItemTitle.'</a></li>';
-}
-$displayFooterMenu1 .= "</ul>";
-
-$displayFooterMenu2 = "<ul>";
-$footerMenu2Items = wp_get_nav_menu_items( 'Secondary Nav' );
-foreach ( $footerMenu2Items as $menu2Item ) {
-	$menu2ItemTitle = $menu2Item->title;
-	$menu2ItemPermalink = $menu2Item->url;
-	$displayFooterMenu2 .= '<li><a href="'.$menu2ItemPermalink.'">'.$menu2ItemTitle.'</a></li>';
-}
-$displayFooterMenu2 .= "</ul>";
-
-echo <<<EOD
 <footer id="ft">
+	<div class="container-1000">
 
-<div class="row">
+	<?php
+	$siteName = get_bloginfo( 'name' );
+		// $displayFooterMenu1 = "<ul class='footer-pn-1'>";
+		// $footerMenuItems = wp_get_nav_menu_items( 'Primary Nav' );
+		// foreach ( $footerMenuItems as $menuItem ) {
+		// 	$menuItemTitle = $menuItem->title;
+		// 	$menuItemPermalink = $menuItem->url;
+		// 	$displayFooterMenu1 .= '<li><a href="'.$menuItemPermalink.'">'.$menuItemTitle.'</a></li>';
+		// }
+		// $displayFooterMenu1 .= "</ul>";
+		// echo $displayFooterMenu1;
 
-<div class="column-flex-1">
+		wp_nav_menu(array('container'=>false,'menu_class'=>'footer-pn-1','menu'=>'Primary Nav'));
 
-	<div class="gutter" style="padding-bottom:0;">
-	<h2 class="last" style="color:#000;">$siteName</h2>
-	<p class="last">$siteDescription</p>
+		wp_nav_menu(array('container'=>false,'menu_class'=>'footer-pn-2','menu'=>'Secondary Nav'));
+	?>
+	<div class="newsletter-wrap">
+		<p>Get <img src="<?php echo get_bloginfo('template_url'); ?>/images/HJM-logo.png"> in your inbox!</p>
+	<?php $subscribeForm = do_shortcode('[mc4wp_form id=1188]');
+		echo $subscribeForm;
+	?>
 	</div>
+	<section style="">
+<div class="row row-on-mobile" style="align-items: center; ">
+<div class="column-flex-1"><div class="gutter" style="text-align:center; padding-bottom:0;"><small class="first last">&copy; <?php echo $siteName; ?></small></div></div>
 
-	<div class="gutter">
-
-	<div class="row row-on-mobile">
-	<div class="column-flex-1" style="margin-right:1rem;">
-	$displayFooterMenu1
-	</div>
-	<div class="column-flex-1">
-	$displayFooterMenu2
-	</div>
-	</div>
-
-	</div>
-
-</div>
-$displaySubscribeForm
-</div>
-
-</footer><!-- /#ft -->
-
-<section style="background:#eee;">
-<div class="row row-on-mobile" style="align-items: center;">
-<div class="column-flex-1"><div class="gutter" style="text-align:left;"><small class="first last">&copy; $siteName</small></div></div>
-<div class="column-flex-1"><div class="gutter" style="text-align:center"><a href="https://www.facebook.com/HealthJusticeMonitor" target="_blank" rel="me"><img src="/wp-content/themes/hjm/images/facebook.svg" alt="Facebook" height="24" width="24"></a> <a href="https://twitter.com/Health__Justice" target="_blank" rel="me"><img src="/wp-content/themes/hjm/images/twitter.svg" alt="Twitter" height="24" width="24"></a></div></div>
-<div class="column-flex-1"><div class="gutter" style="text-align:right;">$displayCredit</div></div>
 </div></div>
 </section>
 
-EOD;
-
-?>
-</div><!-- /#doc -->
-
+	</div>
+</footer>
 <?php wp_footer(); ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
--- wp-content/themes/hjm/front-page.php
+++ wp-content/themes/hjm/front-page.php
@@ -171,7 +171,7 @@ echo '<main id="bd">';
 </div>
 
 
-<section>
+<section class="ai-chat">
 	<div class="gutter">
 		<h2 class="bold">Ask your own question!</h2>
 	<div class="iframe-wrap"><iframe src="https://widget.writesonic.com/CDN/index.html?service-base-url=https://api.botsonic.ai&amp;token=7174aca8-b181-4b2f-b132-406b824fd7bb&amp;base-origin=https://bot.writesonic.com&amp;instance-name=Botsonic&amp;standalone=true&amp;page-url=https://bot.writesonic.com/00db58eb-d803-4d19-a55c-01867f01add9?t=share&amp;workspace_id=4482bd9a-fa91-483c-8768-8d9a7c5d8ebc" frameborder="0">
